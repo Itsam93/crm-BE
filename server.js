9e9969d6c362908479c80a525a525976e612e600
Samuel Oglekwu <106889900+Itsam93@users.noreply.github.com>
2025-10-14 18:01:53 -0700

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: true }));
 // CORS Middleware
 app.use(
   cors({
-    origin: ["http://localhost:5174", "https://crm-app-mkq9.vercel.app/"], 
+    origin: ["http://localhost:5174", "https://crm-app-atjd.vercel.app/"], 
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true, 
